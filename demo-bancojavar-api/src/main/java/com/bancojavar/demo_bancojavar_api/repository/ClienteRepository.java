39c6a680c50320f57e2df10c88d9375d95eb5e85
João Goldoni Ambrósio <125475444+JoaomBRosio@users.noreply.github.com>
2025-01-20 08:46:03 -0300

    Delete demo-bancojavar-api directory

--- demo-bancojavar-api/src/main/java/com/bancojavar/demo_bancojavar_api/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bancojavar.demo_bancojavar_api.repository;
-
-import com.bancojavar.demo_bancojavar_api.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
